fc88f5c854e635a69a9ec8ad228a9bd8f480d95b
Thaina Carvalho <thaina.carvalho@driven.cx>
2021-10-05 17:31:20 -0300

    finaliza calculadora

--- src/calculadora/Calculadora.js
+++ src/calculadora/Calculadora.js
@@ -1,17 +1,62 @@
 import './calculadora.scss';
 import React, { useState } from 'react';
 import { Jumbotron, Container, Row, Col, Button , Form} from 'react-bootstrap'
+import CalculadoraService from './calculadora.service'
 
 function Calculadora() {
 
+  const [ calcular, concatenarNumero , SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO ] = CalculadoraService();
+
   const [txtNumeros, setTxtNumeros] = useState('0')
+  const [numero1, setNumero1] = useState('0')
+  const [numero2, setNumero2] = useState(null)
+  const [operacao, setOperacao] = useState(null)
 
   function adicionarNumero(numero) {
-    setTxtNumeros(txtNumeros + numero)
+    // setTxtNumeros(txtNumeros + numero)
+    let resultado;
+    if(operacao === null){
+      resultado = concatenarNumero(numero1,numero)
+      setNumero1(resultado)
+    } else {
+      resultado = concatenarNumero(numero2, numero)
+      setNumero2(resultado)
+    }
+    setTxtNumeros(resultado)
   }
 
   function definirOperacao(op) {
-    setTxtNumeros(op)
+    //apenas define a operação caso ela não exista
+    if(operacao === null) {
+      setOperacao(op)
+      return;
+    }
+    //caso a operação estiver definida e o numero 2 selecionado, realiza o calculo da operação
+    if(numero2 !== null) {
+      const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao)
+      
+      setOperacao(op);
+      setNumero1(resultado.toString())
+      setNumero2(null);
+      setTxtNumeros(resultado.toString())
+    }
+
+  }
+
+  function acaoCalcular() {
+    if(numero2 === null) {
+      return;
+    }
+
+    const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao)
+    setTxtNumeros(resultado)
+  }
+
+  function limpar() {
+    setTxtNumeros('0')
+    setNumero1('0')
+    setNumero2(null)
+    setOperacao(null)
   }
 
   return (
@@ -21,12 +66,11 @@ function Calculadora() {
         margin: '20px auto',
         width: '400px',
         background: 'pink'
-
       }}>
         <Container>
           <Row>
             <Col xs="3">
-              <Button variant="danger">C</Button>
+              <Button variant="danger" onClick={limpar}>C</Button>
             </Col>
 
             <Col xs="9">
@@ -36,6 +80,7 @@ function Calculadora() {
                 className="text-right"
                 readOnly="readonly"
                 value={txtNumeros}
+                data-testid="txtNumeros"
               />
             </Col>
           </Row>
@@ -51,7 +96,7 @@ function Calculadora() {
               <Button variant="light" onClick={() => adicionarNumero('9')}>9</Button>
             </Col>
             <Col>
-              <Button variant="warning" onClick={() => definirOperacao('/')}>/</Button>
+              <Button variant="warning" onClick={() => definirOperacao(DIVISAO)}>/</Button>
             </Col>
           </Row>
 
@@ -66,7 +111,7 @@ function Calculadora() {
               <Button variant="light" onClick={() => adicionarNumero('6')}>6</Button>
             </Col>
             <Col>
-              <Button variant="warning" onClick={() => definirOperacao('*')}>*</Button>
+              <Button variant="warning" onClick={() => definirOperacao(MULTIPLICACAO)}>*</Button>
             </Col>
           </Row>
 
@@ -81,7 +126,7 @@ function Calculadora() {
               <Button variant="light" onClick={() => adicionarNumero('3')}>3</Button>
             </Col>
             <Col>
-              <Button variant="warning" onClick={() => definirOperacao('-')}>-</Button>
+              <Button variant="warning" onClick={() => definirOperacao(SUBTRACAO)}>-</Button>
             </Col>
           </Row>
 
@@ -90,13 +135,13 @@ function Calculadora() {
               <Button variant="light" onClick={() => adicionarNumero('0')}>0</Button>
             </Col>
             <Col>
-              <Button variant="light">.</Button>
+              <Button variant="light" onClick={() => adicionarNumero('.')}>.</Button>
             </Col>
             <Col>
-              <Button variant="success">=</Button>
+              <Button variant="success" onClick={acaoCalcular}>=</Button>
             </Col>
             <Col>
-              <Button variant="warning" onClick={() => definirOperacao('+')}>+</Button>
+              <Button variant="warning" onClick={() => definirOperacao(SOMA)}>+</Button>
             </Col>
           </Row>
 
--- src/calculadora/calculadora.service.js
+++ src/calculadora/calculadora.service.js
@@ -28,7 +28,26 @@ function CalculadoraService() {
     return resultado
   }
 
-  return [calcular, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO]
+  function concatenarNumero(numAtual, numConcat) {
+    //caso contenha apenas '0' ou null, reinicia o valor
+    if(numAtual === '0' || numAtual === null) {
+      numAtual = ''
+    }
+
+    // quando o primeiro digito for um '.', contatena o '0' antes do '.'
+    if(numConcat === '.' && numAtual === '') {
+      return '0.'
+    }
+    //caso '.' digitado e ja contenha um ponto, apenas retorna
+    if(numConcat === '.' && numAtual.indexOf('.') > -1 ) {
+      return numAtual
+    }
+
+    return numAtual + numConcat;
+
+  }
+
+  return [calcular, concatenarNumero , SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO]
 }
 
 export default CalculadoraService
